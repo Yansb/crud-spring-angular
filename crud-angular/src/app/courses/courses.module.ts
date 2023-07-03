f0aa7434501551e951563e5b23dca103ace67e70
yansb <yansbarreiro@gmail.com>
2023-07-02 23:08:43 -0300

    feat(front): list courses

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
-  imports: [
-    CommonModule,
-    CoursesRoutingModule
-  ]
+  declarations: [CoursesComponent],
+  imports: [CommonModule, CoursesRoutingModule, AppMaterialModule, SharedModule],
 })
-export class CoursesModule { }
+export class CoursesModule {}
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable, catchError, delay, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
+  styleUrls: ['./courses.component.scss'],
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
 
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError(() => {
+        this.onError('Erro ao carregar cursos');
+        return of([]);
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
 }
